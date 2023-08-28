3a7195512b99938e8d6ab0b66de14aba006cfc4f
Joshua Hoblitt <josh@hoblitt.com>
2023-08-28 12:50:41 -0700

    add default_hosts param

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,12 +2,20 @@
 #
 # @param hosts A hash of hosts entries to manage
 #
+# @param default_hosts A hash of hosts entries to manage by default
+#
 class hosts (
   Optional[Hash[String[1], Hash[String[1], Any]]] $hosts = undef,
+  Optional[Hash[String[1], Hash[String[1], Any]]] $default_hosts = undef,
 ) {
-  if $hosts {
-    $hosts.each |$name, $params| {
-      host { $name:
+  $all_hosts = $hosts ? {
+    undef   => $default_hosts,
+    default => $default_hosts + $hosts,
+  }
+
+  if $all_hosts {
+    $all_hosts.each |$n, $params| {
+      host { $n:
         * => $params,
       }
     }
--- spec/acceptance/hosts_spec.rb
+++ spec/acceptance/hosts_spec.rb
@@ -7,5 +7,20 @@ describe 'hosts class' do
     include_examples 'the example', 'no_hosts.pp'
 
     it_behaves_like 'an idempotent resource'
+
+    describe file('/etc/hosts') do
+      %w[
+        127.0.0.1\s+localhost\s+localhost4 localhost4.localdomain4 localhost.localdomain
+        ::1\s+localhost6\s+ip6-localhost ip6-loopback localhost6.localdomain6
+        fe00::0\s+ip6-localnet
+        ff00::0\s+ip6-mcastprefix
+        ff02::1\s+ip6-allnodes
+        ff02::2\s+ip6-allrouters
+      ].each do |entry|
+        its(:content) do
+          is_expected.to match Regexp.new(entry)
+        end
+      end
+    end
   end
 end
